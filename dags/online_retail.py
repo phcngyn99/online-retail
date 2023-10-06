930cf4cb212a350f9048335f8cf2aab9f9ccfcde
phuccnguyen <phuccnguyen99@gmail.com>
2023-10-06 17:21:00 +0700

    Create empty dataset on BQ

--- dags/online_retail.py
+++ dags/online_retail.py
@@ -1,12 +1,14 @@
+import os
 from airflow.decorators import dag, task
 from datetime import datetime
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
 
 @dag(
     start_date= datetime(2023,1,1),
     schedule= None,
     catchup= False,
-    tags= ['online_retail']
+    tags= ['online_retail'] #DAG id
 )
 
 def online_retail():
@@ -19,6 +21,12 @@ def online_retail():
         gcp_conn_id= "gcp",
         mime_type= "text/csv"
     )
-
+    
+    create_bq_dataset= BigQueryCreateEmptyDatasetOperator(
+        task_id = "create_bq_dataset",
+        dataset_id= "online_retail",
+        gcp_conn_id= "gcp"
+    )
+    
 online_retail()
     
