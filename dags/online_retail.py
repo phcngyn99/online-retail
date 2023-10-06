f1a0e358e285915e3e6a2036c48ae9577f3d6014
phuccnguyen <phuccnguyen99@gmail.com>
2023-10-06 18:55:35 +0700

    Transfer file from GCS to BQ

--- dags/online_retail.py
+++ dags/online_retail.py
@@ -3,6 +3,11 @@ from airflow.decorators import dag, task
 from datetime import datetime
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
+project = "cdp-customer-data-platform"
+dataset = "online_retail"
+bucket = "project-dataset-kaggle"
 
 @dag(
     start_date= datetime(2023,1,1),
@@ -17,15 +22,35 @@ def online_retail():
         task_id= "upload_csv_to_gcs",
         src= "/usr/local/airflow/include/dataset/online_retail.csv",
         dst= "raw/online_retail.csv",
-        bucket= "project-dataset-kaggle",
+        bucket= f"{bucket}",
         gcp_conn_id= "gcp",
         mime_type= "text/csv"
     )
     
     create_bq_dataset= BigQueryCreateEmptyDatasetOperator(
         task_id = "create_bq_dataset",
-        dataset_id= "online_retail",
-        gcp_conn_id= "gcp"
+        dataset_id= f"{dataset}",
+        gcp_conn_id= "gcp",
+        if_exists= "ignore"
+    )
+
+    gcs_to_bq = GCSToBigQueryOperator(
+        task_id = "gcs_to_bq",
+        bucket= f"{bucket}",
+        source_objects="raw/online_retail.csv",
+        destination_project_dataset_table= f"{project}.{dataset}.raw",
+        gcp_conn_id= "gcp",
+        schema_fields= [
+            {"name": "InvoiceNo", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "StockCode", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "Description", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "Quantity", "type": "NUMERIC", "mode": "NULLABLE"},
+            {"name": "InvoiceDate", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "UnitPrice", "type": "NUMERIC", "mode": "NULLABLE"},
+            {"name": "CustomerID", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "Country", "type": "STRING", "mode": "NULLABLE"}
+        ],
+        write_disposition= "WRITE_TRUNCATE" #Overwrite if existed
     )
     
 online_retail()
